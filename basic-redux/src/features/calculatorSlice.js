5dedb6d04dcc9f937dd7850a9dc45fb783bf54d3
Silzhar <luis.ruizfer@gmail.com>
2020-10-28 13:28:02 +0100

    Create initialState. Reducers: startAndStop and changeNumber.

--- basic-redux/src/features/calculatorSlice.js
+++ basic-redux/src/features/calculatorSlice.js
@@ -3,5 +3,41 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const calculatorSlice = createSlice({
     name: 'calculator',
+
+    initialState: {
+        isStarted: false,
+        stateCalculator: 0,
+        numbers: [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+          ],
+      
+    },
+
+    reducers: {
+        startAndStop: (state) => {
+            state.isStarted = !state.isStarted
+        },
+
+        changeNumber: (state, action) => {
+            const { row, cell } = action.payload
+            state.numbers[row][cell] = state.stateCalculator
+        }
+    }
 })
 
+// ACTIONS.
+export const { startAndStop, changeNumber } = calculatorSlice.actions
+
+// Selectors. Trae un valor del estado en forma de variable.
+// state : estado de Redux.
+export const selectIsStarted = (state) => {
+    return state.calculator.isStarted
+}
+
+export const selectNumbers = (state) => {
+    return state.calculator.numbers
+}
+
+export default calculatorSlice.reducer
