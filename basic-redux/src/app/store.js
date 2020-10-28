d93440066521f4900aa654efa96267ba645302f7
Silzhar <luis.ruizfer@gmail.com>
2020-10-28 13:24:42 +0100

    add Calculator in store.

--- basic-redux/src/app/store.js
+++ basic-redux/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counterSlice';
+import calculatorReducer from '../features/calculatorSlice'
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    calculator: calculatorReducer,
   },
 });
