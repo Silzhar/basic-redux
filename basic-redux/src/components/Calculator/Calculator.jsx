176ce2d8db866d6942144b89b6484df9f29f42f9
Silzhar <luis.ruizfer@gmail.com>
2020-10-28 13:34:00 +0100

    Create label,start and stop ok.TODO: label numbers

--- basic-redux/src/components/Calculator/Calculator.jsx
+++ basic-redux/src/components/Calculator/Calculator.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { 
+    // Selectors
+    selectIsStarted,
+    selectNumbers,
+    // Actions
+    startAndStop,
+    changeNumber
+ } from '../../features/calculatorSlice'
 
 export function Calculator() {
-    return (
+    const dispatch = useDispatch()
+
+    const isStarted = useSelector(selectIsStarted)
+    const numbers = useSelector(selectNumbers)
+    
+    return (  
+        <nav className="Navbar">
         <div>
-            <h3>Calcularora</h3>
+        <h3>Calcularora</h3>
+
+        <button onClick={() => dispatch(startAndStop())} className="Navbar__playStop">
+            {isStarted ?  'STOP'
+            :  'PLAY' }
+        </button>
         </div>
+
+        {isStarted ? (
+        <div className="Label">
+            {numbers.map((row, rowIndex) => (
+            <div key={rowIndex} className="Label__buttons">
+                {row.map((cellValue, cellIndex) => (
+                <button
+                    className="Label__radioButton"
+                    key={rowIndex + cellIndex}
+                    onClick={() => {
+                    dispatch(
+                        changeNumber({
+                        row: rowIndex,
+                        cell: cellIndex,
+                        })
+                    );
+                    }}
+                >
+                    {cellValue}
+                </button>
+                ))}
+            </div>
+            ))}
+        </div>
+        ) : null}
+        </nav>
+
     )
 }
